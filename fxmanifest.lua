3d71ec13a37dce19107311c2dfb948ccadbd6c09
AhmDevX <ahmedevx4@gmail.com>
2025-06-15 15:50:08 +0500

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+game 'gta5'
+
+lua54 'yes'
+
+author 'DevX - discord.gg/devx-scripts'
+description 'DevX Text UI'
+version '1.0.0'
+
+client_script 'client.lua'
+
+ui_page 'html/index.html'
+
+files 'html/**/*'
+
+exports {
+    'ShowTextUI',
+    'HideTextUI'
+}
